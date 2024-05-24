b6f4e0e5e9e752ff984d65711cf62f96260cc7b0
bereket kume <bereketkume@gmail.com>
2024-05-24 06:07:10 -0700

    stats api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """ api content """
+import models
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
 from models import storage
+from models.base_model import BaseModel
 from flask import jsonify
 from api.v1.views import app_views
 
@@ -8,3 +16,19 @@ from api.v1.views import app_views
 def status():
     """ return status"""
     return jsonify(status="OK")
+
+@app_views.route('/stats', strict_slashes=False)
+def data():
+    """ return number of data in the form dict"""
+    data_num = {
+        'state': State,
+        'user':User,
+        'amenities':Amenity,
+        'cities':City,
+        'places':Place,
+        'reviews':Review
+    }
+    
+    for data in data_num:
+        data_num[data] = storage.count(data_num[data])
+    return jsonify(data_num)
