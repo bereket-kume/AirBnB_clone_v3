c7d09c4bef9a23032089dcd26658082990740387
bereket kume <bereketkume@gmail.com>
2024-05-24 05:26:43 -0700

    return value arranged

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,4 +7,6 @@ from api.v1.views import app_views
 @app_views.route('/status', methods=['GET'])
 def status():
     """ return status"""
-    return jsonify({"status":"OK"})
+    return jsonify({
+        "status": "OK"
+        })
