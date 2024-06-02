958e7d007d16eeec5e9e751fe71988313702e65c
bereket-kume <bereketkume@gmail.com>
2024-06-02 00:23:28 -0700

    fix model error

--- models/state.py
+++ models/state.py
@@ -2,33 +2,33 @@
 """ holds class State"""
 import models
 from models.base_model import BaseModel, Base
-from models.city import City
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
+	"""Representation of state """
+	if getenv('HBNB_TYPE_STORAGE') == 'db':
+		__tablename__ = 'states'
+		id = Column(String(60), primary_key=True, nullable=False)
+		name = Column(String(128), nullable=False)
+		cities = relationship("City", cascade="all, delete", backref="states")
+	else:
+		name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		"""initializes state"""
+		super().__init__(*args, **kwargs)
 
-    if models.storage_t != "db":
-        @property
-        def cities(self):
-            """getter for list of city instances related to the state"""
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+	if getenv('HBNB_TYPE_STORAGE') != 'db':
+		@property
+		def cities(self):
+			"""fs getter attribute that returns City instances"""
+			values_city = models.storage.all("City").values()
+			list_city = []
+			for city in values_city:
+				if city.state_id == self.id:
+					list_city.append(city)
+			return list_city
